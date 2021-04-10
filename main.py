7768e8ec548f1e4b6144f9d8b555417275b6fbb8
astre <astre2019@foxmail.com>
2021-04-10 13:05:12 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8-*-
-from urllib import request
+import urllib.parse
+import urllib.request
 import json
 import random
 import math
@@ -7,7 +8,30 @@ import os
 
 # 影响因素url：详见巨潮资讯API文档：http://webapi.cninfo.com.cn/#/apiDoc
 factor_url = "http://webapi.cninfo.com.cn/api/stock/p_stock2303"
+values = {'status': 'hq', 'token': 'C6AD7DAA24BAA29AE14465DDC0E48ED9'}
 
+# 对请求数据进行编码
+data = urllib.parse.urlencode(values).encode('utf-8')
+print(type(data))   # 打印<class 'bytes'>
+print(data)         # 打印b'status=hq&token=C6AD7DAA24BAA29AE14465DDC0E48ED9'
+
+# 若为post请求以下方式会报错TypeError: POST data should be bytes, an iterable of bytes, or a file object. It cannot be of type str.
+# Post的数据必须是bytes或者iterable of bytes,不能是str,如果是str需要进行encode()编码
+data = urllib.parse.urlencode(values)
+print(type(data))   # 打印<class 'str'>
+print(data)         # 打印status=hq&token=C6AD7DAA24BAA29AE14465DDC0E48ED9
+
+# 将数据与url进行拼接
+req = factor_url + '?' + data
+# 打开请求，获取对象
+response = urllib.request.urlopen(req)
+print(type(response))  # 打印<class 'http.client.HTTPResponse'>
+# 打印Http状态码
+print(response.status) # 打印200
+# 读取服务器返回的数据,对HTTPResponse类型数据进行读取操作
+the_page = response.read()
+# 中文编码格式打印数据
+print(the_page.decode("unicode_escape"))
 
 # # 资产报酬率=息税前利润/平均资产总额 F016N:总资产报酬率
 # # 净资产收益率=ROE=净利润/净资产
